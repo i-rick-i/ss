02da2a6a5bcd4f80cf1aa6ed64308a892b950c22
i-rick-i <83608226+i-rick-i@users.noreply.github.com>
2021-08-07 18:04:06 +0300

    Updates

---
+++ precache-manifest.a99d06f709e91f72db75c831d3f0e6d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6e3f8f870c7cf26b821",
+    "url": "/static/css/main.ec51be36.chunk.css"
+  },
+  {
+    "revision": "d6e3f8f870c7cf26b821",
+    "url": "/static/js/main.af6c1f1c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "16f27df09482a8232d86",
+    "url": "/static/css/2.13a707f4.chunk.css"
+  },
+  {
+    "revision": "16f27df09482a8232d86",
+    "url": "/static/js/2.66413c92.chunk.js"
+  },
+  {
+    "revision": "a98c863e08f62173c1443525e6b9dd05",
+    "url": "/index.html"
+  }
+];
